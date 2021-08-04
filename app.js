4e9fe0e6e8aa9f24555f2e99198b3a0100cc18d1
your name <hiepdqgch18021@fpt.edu.vn>
2021-08-04 15:17:59 +0700

    ok

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const { Console } = require('console');
+
 const express = require('express')
 const app = express()
-app.checkout('/',(req,res)=>{
+app.get('/',(req,res)=>{
        res.sendFile(__dirname + '/views/index.html');
 })
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
-console.log('server is running : ', PORT)
+app.listen(PORT, ()=>{
+       console.log('server is running : ', PORT)
+});
+
 
 
 
