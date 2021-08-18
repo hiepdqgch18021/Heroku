696ae3335e55481e267ed77f9a93d285854872ec
your name <hiepdqgch18021@fpt.edu.vn>
2021-08-18 15:44:43 +0700

    ok

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const app = express()
 app.get('/',(req,res)=>{
-       res.sendFile(__dirname + '/views/Asm.hbs');
+       res.sendFile(__dirname + '/views/Asm.html');
 })
 
 const PORT = process.env.PORT || 5000;
