a7bc6c69bb5d68f4e313b4b0a126c58adc071543
your name <hiepdqgch18021@fpt.edu.vn>
2021-08-21 20:32:41 +0700

    ok

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const app = express()
 const {ObjectId,MongoClient} = require('mongodb');
-
-
 //const MongoClient = require('mongodb');
 const url = 'mongodb://localhost:27017';
-
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 
@@ -16,11 +13,11 @@ app.use(express.urlencoded({ extended: true }))
 //        res.sendFile(__dirname + '/views/view.html');
 // })
 
-app.post('/insert',async(req,res)=>{
-
+app.post('/add',async(req,res)=>{
+       const idInput =req.body.txtId;
        const nameInput = req.body.txtName;
-       const tuoiInput = req.body.txtTuoi;
-       const newStudent = {name: nameInput, tuoi : tuoiInput}
+       const priceInput = req.body.txtPrice;
+       const newStudent = {ID : idInput ,Name: nameInput, Price : priceInput}
 
        const client = await MongoClient.connect(url);
        const dbo = client.db("GCH0805");
