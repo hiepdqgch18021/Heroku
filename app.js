1aa9a0498df39b0724e575d1d1c49901eb1e969f
your name <hiepdqgch18021@fpt.edu.vn>
2021-08-23 10:00:31 +0700

    updat data

--- app.js
+++ app.js
@@ -1,35 +1,22 @@
 const express = require('express');
 const app = express()
 const {ObjectId,MongoClient} = require('mongodb');
+const url = "mongodb+srv://hiepdqgch18021:hiep1234@cluster0.rwpe8.mongodb.net/test";
+
 //const MongoClient = require('mongodb');
-const url = 'mongodb://localhost:27017';
+//const url = 'mongodb://localhost:27017';
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
-
-
-// const {ObjectId,MongoClient} = require('mongodb');
-// const{getDB} = require('./DBhandler')
-// app.get('/',(req,res)=>{
-//        res.sendFile(__dirname + '/views/view.html');
-// })
 
 app.post('/add',async(req,res)=>{
        const idInput =req.body.txtId;
        const nameInput = req.body.txtName;
        const priceInput = req.body.txtPrice;
        const newStudent = {ID : idInput ,Name: nameInput, Price : priceInput}
-
        const client = await MongoClient.connect(url);
        const dbo = client.db("GCH0805");
        await dbo.collection("students").insertOne(newStudent);
        res.redirect("/");
-
-       //const dbo = await getDB();
-//     const nameInput = req.body.txtName;
-//     const tuoiInput = req.body.txtTuoi;
-//     const pictureInput = req.body.txtPicture;
-//     const newStudent = {name:nameInput,tuoi:tuoiInput,picture:pictureInput}
-//  insertStudent(newStudent);
 })
   
 app.get('/delete',async (req,res)=>{
@@ -40,17 +27,11 @@ app.get('/delete',async (req,res)=>{
        await dbo.collection("students").deleteOne({"_id" : ObjectId(id)});
        res.redirect("/");
 
-       // //res.render('index',{data:allStudents})
-       // res.redirect("/");
-       // const id= req.query.id; 
-       // await deleteStudent(id);
 })
 
 app.get('/',async (req,res)=>{
        const client = await MongoClient.connect(url);
-       const dbo = client.db("GCH0805")
-       
-       //const dbo = await getDB(); 
+       const dbo = client.db("GCH0805")      
        const allStudents = await dbo.collection("students").find({}).toArray();
        res.render('index',{data:allStudents})
        return dbo;
