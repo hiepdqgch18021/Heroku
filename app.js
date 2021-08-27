2af8ff1f24471fadcb4fcb555de9964645d993f5
your name <hiepdqgch18021@fpt.edu.vn>
2021-08-27 22:34:55 +0700

    ok

--- app.js
+++ app.js
@@ -2,41 +2,42 @@ const express = require('express');
 const app = express()
 const {ObjectId,MongoClient} = require('mongodb');
 const url = "mongodb+srv://hiepdqgch18021:hiep1234@cluster0.rwpe8.mongodb.net/test";
-
-//const MongoClient = require('mongodb');
-//const url = 'mongodb://localhost:27017';
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
 
 app.post('/add',async(req,res)=>{
-       const idInput =req.body.txtId;
+       const quantityInput = req.body.txtQuantity;
        const nameInput = req.body.txtName;
        const priceInput = req.body.txtPrice;
-       const newStudent = {ID : idInput ,Name: nameInput, Price : priceInput}
+       const newProduct = {Name: nameInput, Price : priceInput, Quantity : quantityInput}
        const client = await MongoClient.connect(url);
-       const dbo = client.db("GCH0805");
-       await dbo.collection("students").insertOne(newStudent);
+       const dbo = client.db("AsmDB2");
+       await dbo.collection("Asm").insertOne(newProduct);
        res.redirect("/");
 })
   
 app.get('/delete',async (req,res)=>{
        const id  =req.query.id;
-       // var ObjectID = require('mongodb').ObjectId;
        const client = await MongoClient.connect(url);
-       const dbo = client.db("GCH0805");
-       await dbo.collection("students").deleteOne({"_id" : ObjectId(id)});
+       const dbo = client.db("AsmDB2");
+       await dbo.collection("Asm").deleteOne({"_id" : ObjectId(id)});
        res.redirect("/");
-
 })
-
+app.post('/search',async (req,res)=>{
+        const searchInput = req.body.txtSearch;
+        const client = await MongoClient.connect(url);
+        const dbo = client.db("AsmDB2");
+        const allProducts = await dbo.collection("Asm").find({Name : searchInput}).toArray();
+        res.render('index',{data:allProducts})
+})
 app.get('/',async (req,res)=>{
        const client = await MongoClient.connect(url);
-       const dbo = client.db("GCH0805")      
-       const allStudents = await dbo.collection("students").find({}).toArray();
-       res.render('index',{data:allStudents})
+       const dbo = client.db("AsmDB2")      
+       const allProducts = await dbo.collection("Asm").find({}).toArray();
+       res.render('index',{data:allProducts})
        return dbo;
 })
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>{
