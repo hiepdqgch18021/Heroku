ea52fff68b497e7de46aa8e888f9908f20d25be7
your name <hiepdqgch18021@fpt.edu.vn>
2021-08-28 16:40:52 +0700

    port update

--- app.js
+++ app.js
@@ -40,9 +40,9 @@ app.get('/',async (req,res)=>{
 })
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, ()=>{
-       console.log('server is running : ', PORT)
-});
+app.listen(PORT);
+console.log("app is running : ", PORT);
+
 
 
 
